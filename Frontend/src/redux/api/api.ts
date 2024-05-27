b1483a17b47ea31ff17d1afcd34abf62298ddb79
Fka <test@gmail.com>
2024-05-27 08:29:07 +0300

    'first'

--- Frontend/src/redux/api/api.ts
+++ Frontend/src/redux/api/api.ts
@@ -1,6 +1,6 @@
 
-//const API_URL = "http://localhost:5000/api/";
-const API_URL = "https://backend-1-ia3n.onrender.com/api/"
+const API_URL = "http://localhost:5000/api/";
+//const API_URL = "https://backend-1-ia3n.onrender.com/api/"
 
 class ApiService {
   getApi() {
